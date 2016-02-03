b6aebfaebd7d9a78e4773b5de7a897866ecd733e
Eric Crescioni <ecam900@gmail.com>
2016-02-03 22:38:18 +0000

    Add nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Showerthoughts::Application.routes.draw do
 
   root 'thoughts#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
