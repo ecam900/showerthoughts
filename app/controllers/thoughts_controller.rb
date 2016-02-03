e4a5f6f824b081c143b458918613ff3d809b7383
Eric Crescioni <ecam900@gmail.com>
2016-02-03 23:24:58 +0000

    Add form functionality

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,5 +1,24 @@
 class ThoughtsController < ApplicationController
 	def index
-		@thought = Thought.last
+		@thought = Thought.order("RANDOM()").first
 	end
+
+	def new
+		@thought = Thought.new
+	end
+
+	def create
+		Thought.create(quote_params)
+		redirect_to root_path
+	end
+
+
+  private
+
+  def quote_params
+    params.require(:thought).permit(:idea)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Showerthoughts::Application.routes.draw do
 
   root 'thoughts#index'
 
+  resources :thoughts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
